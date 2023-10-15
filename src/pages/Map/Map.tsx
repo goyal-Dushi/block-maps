aa2280e5c1843fd53df4ce05f3512c4f9367fdd7
goyal-Dushi <dushyantgoyal28@gmail.com>
2023-10-15 15:00:06 +0530

    added canvas and update element position over map

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -5,7 +5,10 @@ import { DblockConfig } from '../../maps/sector27/Dblock'
 import { solve } from '../../utils/createBlockMatrix';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { StructureTypes } from './components/structure/Structure';
-import MapForm from './components/mapForm/MapForm';
+import SearchIcon from '../../assets/svg/Search';
+import FormCanvas from './components/mapForm/FormCanvas';
+import BackIcon from '../../assets/svg/BackIcon';
+import StructureBadgeRow from './components/structureBadges/StructureBadgeRow';
 
 interface AppProps { }
 
@@ -13,7 +16,13 @@ const App: React.FC<AppProps> = () => {
   const params = useSearchParams();
   const navigate = useNavigate();
   const [pathSet, setPathHash] = useState<Set<string>>();
-  const [strctType, setStructType] = useState<StructureTypes>();
+  const [strctType, setStructType] = useState<StructureTypes | undefined>();
+  const [showCanvas, setShowCanvas] = useState(false);
+  const searchParams = useSearchParams();
+
+  const destnVal = useMemo(() => {
+    return searchParams[0].get('destn');
+}, [searchParams]);
   
   const dBLockConfig = useMemo(() => {
     const rows = DblockConfig.length;
@@ -35,24 +44,56 @@ const App: React.FC<AppProps> = () => {
     if (!payload.block || !payload.sector) {
       navigate("/getMap");
     }
-
-    if (payload.type) {
-      setStructType(payload.type as StructureTypes);
+    
+    if (payload.type || payload.type === undefined) {
+      setStructType(payload.type as any);
     }
 
     if (payload.src && payload.destn) {
       const set = solve(payload.src, payload.destn, payload.path);
       setPathHash(set);
+    } else {
+      setPathHash(undefined);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [payload]);
 
+  const handleCanvas = () => {
+    setShowCanvas((prev) => {
+      return !prev;
+    });
+  };
+
+  const handleInputClick = () => {
+    handleCanvas();
+  }
+
   return (
     <div className='page-wrapper'>
       <div className='map-window'>
+      {destnVal ? (
+        <>
+          <button type='button' onClick={handleCanvas} className="btn btn-secondary rounded-circle position-fixed" style={{ top: '1rem', left: '1rem', zIndex: 150 }}>
+            <BackIcon />
+          </button>
+        </>
+      ) : (
+        <>
+        <div className="input-group mb-3 position-fixed" style={{ top: '1.8rem', left: "10%", width: "80%", zIndex: "150" }}>
+                <span className="input-group-text" id="basic-addon1">
+                    <SearchIcon />
+                </span>
+                <input value={destnVal || ''} readOnly onClick={handleInputClick} type="number" className="form-control" placeholder="Find House Number" aria-label="find" aria-describedby="basic-addon1" />
+            </div>
+          </>
+      )}
+      <StructureBadgeRow />
         <BlockMap arrangement={DblockConfig} type={strctType} src={payload.src} destn={payload.destn} path={pathSet} dimension={{ ...dBLockConfig }} />
       </div>
-      <MapForm />
+      {showCanvas && (
+        <FormCanvas handleCanvas={handleCanvas} />
+      )}
+      
     </div>
   )
 }
