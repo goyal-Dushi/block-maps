7eb95e4ab4821834eaf55760909168b642438ba7
goyal-Dushi <dushyantgoyal28@gmail.com>
2025-03-23 18:23:12 +0530

    update : update asset file and added new svg

--- src/assets/index.ts
+++
@@ -1,7 +0,0 @@
-// svg exports
-export { default as BackSvg } from "./svg/BackIcon";
-export { default as HouseSvg } from "./svg/HouseIcon";
-export { default as RecenterSvg } from "./svg/Recenter";
-export { default as SearchSvg } from "./svg/Search";
-export { default as ZoominSvg } from "./svg/ZoomIn";
-export { default as ZoomoutSvg } from "./svg/ZoomOut";
