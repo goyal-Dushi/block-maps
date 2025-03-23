88a3c250b12a2502f35b98e7a9c92bea5cc5336f
goyal-Dushi <dushyantgoyal28@gmail.com>
2025-03-23 18:37:36 +0530

    remove cooridnates from path

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Map from './pages/Map/Map.tsx'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-import './index.scss'
-import ErrorPageURL from './error/Error.tsx';
-import { mapFormAction } from './pages/Map/utils.ts';
-import Coordinate from './Coordinate.tsx';
-import { DblockConfig } from './maps/sector27/Dblock.ts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Map from "./pages/Map/Map.tsx";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.scss";
+import ErrorPageURL from "./error/Error.tsx";
+import { mapFormAction } from "./pages/Map/utils.ts";
+import { DblockConfig } from "./maps/sector27/Dblock.ts";
 
 const router = createBrowserRouter([
   {
-    path: '/map',
+    path: "/map",
     element: <Map />,
     action: mapFormAction,
     errorElement: <ErrorPageURL />,
   },
-  {
-    path: '/getCords',
-    element: <Coordinate mapConfig={DblockConfig} />,
-    errorElement: <ErrorPageURL />,
-  },
+  // {
+  //   path: '/getCords',
+  //   element: <Coordinate mapConfig={DblockConfig} />,
+  //   errorElement: <ErrorPageURL />,
+  // },
   {
     path: "*",
-    element: <Navigate to={{
-      pathname: '/map',
-      search: '?block=D&sector=27'
-    }}  replace />
-  }
-])
+    element: (
+      <Navigate
+        to={{
+          pathname: "/map",
+          search: "?block=D&sector=27",
+        }}
+        replace
+      />
+    ),
+  },
+]);
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
